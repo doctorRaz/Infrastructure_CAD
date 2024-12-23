a5052c037d4a38127ccba18c2c42d421a8bdab72
kpblc2000 <kpblc2000@gmail.com>
2024-12-23 22:05:33 +0300

    Дополнил тесты для int

---
+++ Tests/Infrastructure.Tests/IntExtensionsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Extensions;
+using NUnit.Framework;
+
+namespace Infrastructure.Tests
+{
+    public class IntExtensionsTest
+    {
+        [Test]
+        public void DeclensionGeneratorTest()
+        {
+            int value = 1;
+            string res = value.DeclensionGenerator("день", "дня", "дней");
+            Assert.AreEqual(res, "день");
+            value = 11;
+            res = value.DeclensionGenerator("день", "дня", "дней");
+            Assert.AreEqual(res, "дней");
+            value = 21;
+            res = value.DeclensionGenerator("день", "дня", "дней");
+            Assert.AreEqual(res, "день");
+            value = 111;
+            res = value.DeclensionGenerator("день", "дня", "дней");
+            Assert.AreEqual(res, "дней");
+        }
+    }
+}
